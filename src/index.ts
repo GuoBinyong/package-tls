b4070d7d7c0a359b165491947316d35d4eff66fd
郭斌勇 <guobinyong@qq.com>
2021-02-06 18:31:55 +0800

    fix: 修复手误

--- src/index.ts
+++ src/index.ts
@@ -64,7 +64,7 @@ function stringTagWithSpace(space: string | number | undefined | null,strPieces:
 	const allStrPieces = interpolation.reduce(function(allStrs,arg,index){
 		allStrs.push(toString(arg,space),strPieces[index + 1]);
 		return allStrs;
-	},[strPieces[0]]);[]
+	},[strPieces[0]]);
 	return allStrPieces.join("");
 }
 
